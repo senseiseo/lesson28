08dc25ab12a77ee7b5b61404f9ba1501181681ad
ildar <1919mur@gmail.com>
2019-04-23 18:25:15 +0300

    //add details comment

--- app.rb
+++ app.rb
@@ -47,3 +47,12 @@ post '/new' do
 	redirect to('/')
 	erb 
 end 
+
+get '/details/:post_id' do  
+	post_id = params[:post_id]
+
+	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	@row = results[0]
+
+	erb :details
+end 
